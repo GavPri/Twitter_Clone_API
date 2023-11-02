07cde0fe7424feb7ad88c1bca70e9e17eb4961ab
德华 刘 <gavinprior5@gmail.com>
2023-11-02 22:35:23 +0000

    import: ProfileListSerializer to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
+from .serializers import AccountListSerializer
 # Create your views here.
 
 
