4bb1aec309591dff8a9a8688c0664156e89c614a
德华 刘 <gavinprior5@gmail.com>
2023-11-02 22:12:11 +0000

    create: view to list accounts

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
 # Create your views here.
+
+
+# Get request to view account list. 
+class AccountList(APIView):
+    def get(self, request):
+        accounts = Accounts.objects.all()
+        return Response(accounts)
