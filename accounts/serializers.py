c969009ab06a4a0b4c75fc24212326dfccfa4855
德华 刘 <gavinprior5@gmail.com>
2023-11-02 22:34:32 +0000

    create: account list serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,11 @@
 from rest_framework import serializers
 from .models import Account
 
+
+# Accounts List Serializer
+class AccountListSerializer(serializers.ModelSerializer):
+    owner =serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Account
+        fields = '__all__'
