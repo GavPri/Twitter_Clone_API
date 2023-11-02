5d3492e02e60e2ff2a0df02773ab1e3aa8cf6e70
德华 刘 <gavinprior5@gmail.com>
2023-11-02 22:54:48 +0000

    create: account detail view to handle 404 errors

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
 from .serializers import AccountListSerializer
+from django.http import Http404
 # Create your views here.
 
 
-# Get request to view account list. 
 class AccountList(APIView):
+    # Get request to view account list. 
     def get(self, request):
         accounts = Account.objects.all()
         # Serializer instance 
         serializer = AccountListSerializer(accounts, many=True)
         return Response(serializer.data)
+
+
+class AccountDetail(APIView):
+    # Function to handle request for account that does not exists.
+    def get_object(self, pk):
+        try:
+            account = Account.objects.get(pk=pk)
+            return Account
+        except Account.DoesNotExist:
+            raise Http404
