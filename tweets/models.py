36d31ff59f9251bd229476af638337f108b6e805
德华 刘 <gavinprior5@gmail.com>
2023-11-03 00:45:07 +0000

    create: Tweet model, meta & dunder

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# Tweet/Post model
+class Tweet(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE);
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.CharFields(max_length=300)
+    image = models.ImageField(
+        upload_to='images/' , blank=True
+    )
+
+    class Meta: 
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.content[:20]}'
