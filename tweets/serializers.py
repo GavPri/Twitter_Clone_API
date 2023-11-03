22e1a0d04990b278476c5d63789c7e7793d3edf4
德华 刘 <gavinprior5@gmail.com>
2023-11-03 00:54:51 +0000

    add: get is owner function

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -8,3 +8,8 @@ class TweetListSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    # Check for ownership
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
