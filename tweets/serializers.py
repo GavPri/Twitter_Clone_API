4a947edae84ee0e873b5277644689495921de7da
德华 刘 <gavinprior5@gmail.com>
2023-11-03 03:12:10 +0000

    tweet push

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -9,12 +9,14 @@ class TweetListSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+
+
     # Check for ownership
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-
+    # Meta Classes 
     class Meta:
         model = Account
         fields = [
