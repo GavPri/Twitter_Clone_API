15f2070654279cd40ab75961c26e2f551f82af5f
德华 刘 <gavinprior5@gmail.com>
2023-11-03 00:52:22 +0000

    add: owner, is_owner, profile id & profile img to model

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from .models import Tweet
+
+
+# Tweet List Serializer
+class TweetListSerializer(serializers.ModelSerializer):
+    owner =serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
