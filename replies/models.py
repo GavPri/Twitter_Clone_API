c02fdc905231c556f560b14bb469b3027e2b8912
德华 刘 <gavinprior5@gmail.com>
2023-11-08 00:58:40 +0000

    add: image field to replies

--- replies/models.py
+++ replies/models.py
@@ -9,6 +9,9 @@ class Replies(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.CharField(max_length=288)
+    image = models.ImageField(
+        upload_to='images/' , blank=True
+    )
     class Meta:
         ordering = ['-created_at']
 
