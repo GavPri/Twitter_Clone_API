d402201bd082fa7d45a1450fced6b749bf2aa75e
德华 刘 <gavinprior5@gmail.com>
2023-11-08 00:45:57 +0000

    create: generic list/create view for replies

--- replies/views.py
+++ replies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from twitter_clone_api.permissions import IsOwnerOrReadOnly
+from .models import Replies
+from .serializers import RepliesSerializer, RepliesDetailSerializers
 
 # Create your views here.
+
+class ReplyList(generics.ListCreateAPIView):
+    serializer_class = RepliesSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Replies.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
