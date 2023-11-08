73eea2e2860e7c905358c97dd0b6d3fea6e243b9
德华 刘 <gavinprior5@gmail.com>
2023-11-08 23:58:35 +0000

    add: Followers & following id to account serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,21 +1,29 @@
 from rest_framework import serializers
 from .models import Account
+from followers.models import Followers
 
 
 # Accounts List Serializer
 class AccountListSerializer(serializers.ModelSerializer):
-    owner =serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     # Check for profile ownership
     def get_is_owner(self, obj):
-        request = self.context['request']
+        request = self.context["request"]
         return request.user == obj.owner
-
 
     class Meta:
         model = Account
         fields = [
-          'id', 'owner', 'created_at', 'updated_at',
-          'name', 'content', 'image', 'is_owner'
+            "id",
+            "owner",
+            "created_at",
+            "updated_at",
+            "name",
+            "content",
+            "image",
+            "is_owner",
+            "following_id",
         ]
