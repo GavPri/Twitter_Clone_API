b0741f4a50628bf878bb5a89f68bf1e2088eeb09
德华 刘 <gavinprior5@gmail.com>
2023-11-08 00:26:48 +0000

    create: replies model

--- replies/models.py
+++ replies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from tweets.models import Tweet
 # Create your models here.
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=288)
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
