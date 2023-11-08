f817ee9040a3b8854d48e15254ebea0c977b256c
德华 刘 <gavinprior5@gmail.com>
2023-11-08 23:23:08 +0000

    create: follower list view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from twitter_clone_api.permissions import IsOwnerOrReadOnly
+from .models import Followers
+from .serializers import FollowerSerializer
 
 # Create your views here.
+
+
+class FollowersList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Followers.objects.all()
