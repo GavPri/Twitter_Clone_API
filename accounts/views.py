b5204ddcbb23daadd7fda25c20f33b2ec82cd530
德华 刘 <gavinprior5@gmail.com>
2023-11-09 19:51:57 +0000

    add: filter backends to account views

--- accounts/views.py
+++ accounts/views.py
@@ -8,11 +8,19 @@ from django.db.models import Count
 
 class AccountList(generics.ListAPIView):
     queryset = accounts = Account.objects.annotate(
-        tweet_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+        tweet_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
     serializer_class = AccountListSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        "tweet_count",
+        "followers_count",
+        "following_count",
+        "owner__following__created_at",
+        "owner__followed__created_at",
+    ]
 
 
 class AccountDetail(generics.RetrieveUpdateAPIView):
