31543431e996f83ae65a2d1eb81a10f119991d2e
德华 刘 <gavinprior5@gmail.com>
2023-11-09 19:47:26 +0000

    add: tweet, following and followed to accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
-from rest_framework import generics
-from rest_framework.views import APIView
+from rest_framework import generics, filters
 from .models import Account
 from .serializers import AccountListSerializer
 from twitter_clone_api.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 # Create your views here.
 
 
 class AccountList(generics.ListAPIView):
-    queryset = accounts = Account.objects.all()
+    queryset = accounts = Account.objects.annotate(
+        tweet_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = AccountListSerializer
 
 
