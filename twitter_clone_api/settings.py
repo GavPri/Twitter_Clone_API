49f75717faf565218921d5010a72527434e4393c
德华 刘 <gavinprior5@gmail.com>
2023-11-10 05:27:16 +0000

    deployment configuration

--- twitter_clone_api/settings.py
+++ twitter_clone_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -64,7 +65,7 @@ DEBUG = "DEV" in os.environ
 ALLOWED_HOSTS = [
     "8000-gavpri-twittercloneapi-hoi72s6puk8.ws-us106.gitpod.io",
     "localhost",
-    "twitter-ci-4eb0a43581b7.herokuapp.com",
+    os.environ.get("ALLOWED_HOSTS"),
 ]
 
 
@@ -108,11 +109,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
-else:
+if "CLIENT_ORIGIN_DEV" in os.environ:
+    extracted_url = re.match(
+        r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
