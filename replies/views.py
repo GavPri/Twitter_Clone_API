ce9dbac49b15a72651a498d9b67d4ac427e4a09f
德华 刘 <gavinprior5@gmail.com>
2023-11-10 00:53:24 +0000

    install: dj rest auth

--- replies/views.py
+++ replies/views.py
@@ -2,13 +2,17 @@ from rest_framework import generics, permissions
 from twitter_clone_api.permissions import IsOwnerOrReadOnly
 from .models import Replies
 from .serializers import RepliesSerializer, RepliesDetailSerializers
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
 
 class ReplyList(generics.ListCreateAPIView):
     serializer_class = RepliesSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Replies.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["tweet"]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
