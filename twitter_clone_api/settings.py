84e7bc53e41fabfc87866922f8fc42619e032f7e
德华 刘 <gavinprior5@gmail.com>
2023-11-10 05:06:36 +0000

    config allowed hosts

--- twitter_clone_api/settings.py
+++ twitter_clone_api/settings.py
@@ -62,7 +62,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
-    "8000-gavpri-twittercloneapi-hoi72s6puk8.ws-us106.gitpod.io, 'localhost', 'twitter-ci-4eb0a43581b7.herokuapp.com']"
+    "8000-gavpri-twittercloneapi-hoi72s6puk8.ws-us106.gitpod.io",
+    "localhost",
+    "twitter-ci-4eb0a43581b7.herokuapp.com",
 ]
 
 
