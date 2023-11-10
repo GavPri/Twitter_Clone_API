668b3bc55f1b095a3288262aebf4165080a5551a
德华 刘 <gavinprior5@gmail.com>
2023-11-10 04:43:07 +0000

    deployment commit

--- twitter_clone_api/settings.py
+++ twitter_clone_api/settings.py
@@ -46,6 +46,7 @@ if "DEV" not in os.environ:
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = "my-refresh-token"
+JWT_AUTH_SAMESITE = "None"
 
 REST_AUTH_SERIALIZERS = {
     "USER_DETAILS_SERIALIZERS": "twitter_clone_api.CurrentUserSerializer"
@@ -55,12 +56,14 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tz92hzff)w&(3w=-%oi2#md6wrz38xg0+x90ta)3*#)37%f^&y"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEV" in os.environ
 
-ALLOWED_HOSTS = ["8000-gavpri-twittercloneapi-hoi72s6puk8.ws-us106.gitpod.io"]
+ALLOWED_HOSTS = [
+    "8000-gavpri-twittercloneapi-hoi72s6puk8.ws-us106.gitpod.io, 'localhost', 'https://git.heroku.com/twitter-ci.git.herokuapp.com']"
+]
 
 
 # Application definition
@@ -83,6 +86,8 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "dj_rest_auth.registration",
+    "dj_rest_auth.registration",
+    "corsheaders",
     "accounts",
     "tweets",
     "replies",
@@ -92,6 +97,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -100,6 +106,15 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "twitter_clone_api.urls"
 
