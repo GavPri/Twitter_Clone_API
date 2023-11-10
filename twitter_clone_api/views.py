eff4567535b3fc521a8840cb550cb9eee551a2f8
德华 刘 <gavinprior5@gmail.com>
2023-11-10 01:32:32 +0000

    fix: root route error

--- twitter_clone_api/views.py
+++ twitter_clone_api/views.py
@@ -2,6 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view
+@api_view()
 def root_route(request):
     return Response({"message": "Welcome to the api"})
