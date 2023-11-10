1ac2e325683698ae5a7241f9eb8ad8ced1e0bf25
德华 刘 <gavinprior5@gmail.com>
2023-11-10 01:20:47 +0000

    add: api serializers

---
+++ twitter_clone_api/serializers.py
@@ -0,0 +1,10 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    account_id = serializers.ReadOnlyField(source="account.id")
+    account_id = serializers.ReadOnlyField(source="account.image.url")
+
+    class Meta:
+        fields = UserDetailsSerializer.Meta.fields + ("account_id", "account_image")
