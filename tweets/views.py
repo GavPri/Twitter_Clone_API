63f5d55a3e2f90dac081d392b6b10192ad6d4bae
德华 刘 <gavinprior5@gmail.com>
2023-11-10 00:07:53 +0000

    add: filterset fields to tweets

--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,7 @@ from .models import Tweet
 from .serializers import TweetListSerializer
 from twitter_clone_api.permissions import IsOwnerOrReadOnly
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # Create your views here.
@@ -13,17 +14,27 @@ class TweetList(generics.ListCreateAPIView):
         likes_count=Count("likes", distinct=True),
         replies_count=Count("replies", distinct=True),
     ).order_by("-created_at")
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        # user feed
+        "owner__followed__owner__account",
+        # user liked post
+        "likes__owner__account",
+        # user profile
+        "owner__account",
+    ]
     ordering_fields = [
         "likes_count",
         "replies_count",
         "likes__created_at",
         "replies__created_at",
     ]
-    search_fields = [
-        'owner__username',
-        'content'
-    ]
+
+    search_fields = ["owner__username", "content"]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.user.request)
