c2207dec1a5a899ebcdf8a5b4c043e55496abca3
德华 刘 <gavinprior5@gmail.com>
2023-11-11 05:42:25 +0000

    update registration url

--- twitter_clone_api/urls.py
+++ twitter_clone_api/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("dj-rest-auth/logout/", logout_route),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("dj_rest_auth/registration", include("dj_rest_auth.registration.urls")),
+    path("dj-rest-auth/registration", include("dj_rest_auth.registration.urls")),
     path("", include("accounts.urls")),
     path("", include("tweets.urls")),
     path("", include("replies.urls")),
