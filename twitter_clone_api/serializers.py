15d54f01b9d29b03a2df94b7690743189153d827
德华 刘 <gavinprior5@gmail.com>
2023-11-16 06:19:58 +0000

    update serializer

--- twitter_clone_api/serializers.py
+++ twitter_clone_api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     account_id = serializers.ReadOnlyField(source="account.id")
-    account_id = serializers.ReadOnlyField(source="account.image.url")
+    account_image = serializers.ReadOnlyField(source="account.image.url")
 
     class Meta:
         fields = UserDetailsSerializer.Meta.fields + ("account_id", "account_image")
