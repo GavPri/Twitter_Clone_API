2f8d57cdc325329532c29212f1c8b9a48299ecd1
德华 刘 <gavinprior5@gmail.com>
2023-12-01 18:32:12 +0000

    add user detaile meta to serializers.py

--- twitter_clone_api/serializers.py
+++ twitter_clone_api/serializers.py
@@ -6,5 +6,5 @@ class CurrentUserSerializer(UserDetailsSerializer):
     account_id = serializers.ReadOnlyField(source="account.id")
     account_image = serializers.ReadOnlyField(source="account.image.url")
 
-    class Meta:
+    class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + ("account_id", "account_image")
