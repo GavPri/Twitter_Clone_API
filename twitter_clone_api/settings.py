4c7ab9f386a020c6c50e0a800c4fce16c85babfd
德华 刘 <gavinprior5@gmail.com>
2023-12-01 17:28:21 +0000

    correct rest framework typo in settings.py

--- twitter_clone_api/settings.py
+++ twitter_clone_api/settings.py
@@ -26,7 +26,7 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-REST_FRAMEOWRK = {
+REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         (
             "rest_framework.authentication.SessionAuthentication"
